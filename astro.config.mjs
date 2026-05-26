3cf2de7c643316d9a2c48259b15d897999358b02
ptnguyen111 <ptnguyen111@gmail.com>
2026-05-26 11:49:40 +0700

    astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://deploy-preview-5--astro-tutorial-blog-01.netlify.app/',
+  site: 'https://deploy-preview-5--astro-tutorial-blog-01.netlify.app/',
+  integrations: [preact()],
 });
